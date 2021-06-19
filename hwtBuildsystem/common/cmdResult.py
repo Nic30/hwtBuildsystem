e0563d1d3047c44a1ea3e650c4d44f456e0fda27
Nic30 <nic30@seznam.cz>
2021-06-19 13:22:18 +0200

    TclCmdResult: support for terminal colored texts

--- hwtBuildsystem/common/cmdResult.py
+++ hwtBuildsystem/common/cmdResult.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 
 class TclToolErr(Exception):
@@ -11,11 +12,13 @@ class TclCmdResult():
     """
     Parsed result of comand over cli
     """
+    ANY_COLOR = '((\033\[0m)?(\033\[0;(\d+)m)?\s*)?'
+    OPT_MSG_NUMBER = '(\s*\(\d+\))?'
     regex_invalidCmd = re.compile("(invalid command name \".*\")")
-    regex_err = re.compile("ERROR: (.*)")
-    regex_critWarn = re.compile('CRITICAL WARNING: (.*)')
-    regex_warn = re.compile("WARNING: (.*)")
-    regex_info = re.compile('INFO: (.*)')
+    regex_err = re.compile(f"{ANY_COLOR:s}ERROR{OPT_MSG_NUMBER:s}: (.*)")
+    regex_critWarn = re.compile(f"{ANY_COLOR:s}CRITICAL WARNING{OPT_MSG_NUMBER:s}: (.*)")
+    regex_warn = re.compile(f"{ANY_COLOR:s}WARNING{OPT_MSG_NUMBER:s}: (.*)")
+    regex_info = re.compile(f"{ANY_COLOR:s}INFO{OPT_MSG_NUMBER:s}: (.*)")
 
     def __init__(self, cmd, resultText, errors, criticalWarnings, warnings, infos):
         self.cmd = cmd
@@ -26,9 +29,9 @@ class TclCmdResult():
         self.infos = infos
 
     @staticmethod
-    def extractMsgs(msg, regex, listOfMsgs):
+    def extractMsgs(msg, regex, err_str_group_i:int, listOfMsgs: List[str]):
         for m in regex.finditer(msg):
-            listOfMsgs.append(m.group(1).strip())
+            listOfMsgs.append(m.group(err_str_group_i).strip())
         return regex.sub("", msg)
 
     @classmethod
@@ -39,11 +42,11 @@ class TclCmdResult():
         warnings = []
         infos = []
 
-        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_invalidCmd, errors)
-        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_err, errors)
-        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_critWarn, criticalWarnings)
-        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_warn, warnings)
-        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_info, infos)
+        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_invalidCmd, 1, errors)
+        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_err, 6, errors)
+        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_critWarn, 6, criticalWarnings)
+        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_warn, 6, warnings)
+        resultText = TclCmdResult.extractMsgs(resultText, TclCmdResult.regex_info, 6, infos)
 
         return cls(cmd, resultText.strip(), errors, criticalWarnings, warnings, infos)
 
