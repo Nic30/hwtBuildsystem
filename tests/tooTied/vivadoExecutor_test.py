d0e2e5932f116995109a88fe37f6e0e6be68567a
Nic30 <nic30@seznam.cz>
2021-06-29 23:31:49 +0200

    update VivadoExecutorTC

--- tests/tooTied/vivadoExecutor_test.py
+++ tests/tooTied/vivadoExecutor_test.py
@@ -6,33 +6,32 @@ from unittest.case import TestCase
 
 from hwtBuildsystem.common.cmdResult import TclToolErr
 from hwtBuildsystem.vivado.executor import VivadoExecutor
-from hwtBuildsystem.vivado.tcl import VivadoTCL
+from hwtBuildsystem.vivado.api.tcl import VivadoTCL
 
 
 class VivadoExecutorTC(TestCase):
 
-    def runCmds(self, cmds):
-        with VivadoExecutor() as v:
-            for _ in v.process(cmds):
-                pass
-
     def test_VivadoErrorPropagation(self):
-        self.assertRaises(TclToolErr, lambda: self.runCmds(['dir', 'invalid_cmd_test']))
+        with self.assertRaises(TclToolErr):
+            with VivadoExecutor() as v:
+                v.exeCmd('dir')
+                v.exeCmd('invalid_cmd_test')
 
     def test_VivadoErrorValidMsg(self):
         errRes = None
-        try:
-            self.runCmds(['dir', 'invalid_cmd_test'])
-        except TclToolErr as e:
-            errRes = e.cmdResult
-        self.assertTrue(errRes.resultText == '')
-        self.assertTrue(errRes.errors[0] == 'invalid command name "invalid_cmd_test"')
+        with VivadoExecutor() as v:
+            try:
+                v.exeCmd('dir')
+                v.exeCmd('invalid_cmd_test')
+            except TclToolErr as e:
+                errRes = e.args[0]
+
+            self.assertTrue(errRes.resultText == '')
+            self.assertTrue(errRes.errors[0] == 'invalid command name "invalid_cmd_test"')
 
     def test_warningParsing(self):
         with VivadoExecutor() as v:
-            res = list(v.process(['dir']))
-            self.assertEqual(len(res), 1)
-            res = res[0]
+            res = v.exeCmd('dir')
             self.assertEqual(len(res.errors), 0)
             self.assertEqual(len(res.criticalWarnings), 0)
             self.assertEqual(len(res.warnings), 1)
@@ -40,8 +39,9 @@ class VivadoExecutorTC(TestCase):
 
     def test_ls(self):
         with VivadoExecutor() as v:
-            _pwd, _dir = v.process([VivadoTCL.pwd(), VivadoTCL.ls()])
-            ls = os.listdir(_pwd.resultText)
+            _pwd = v.exeCmd(VivadoTCL.pwd())
+            _dir = v.exeCmd(VivadoTCL.ls())
+            ls = [f for f in os.listdir(_pwd.resultText) if not f.startswith('.')]
             vivadoLs = _dir.resultText.split()
             ls.sort()
             vivadoLs.sort()
@@ -50,7 +50,7 @@ class VivadoExecutorTC(TestCase):
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    # suite.addTest(ControllerTC("test_VivadoErrorValidMsg"))
+    # suite.addTest(VivadoExecutorTC("test_VivadoErrorValidMsg"))
     suite.addTest(unittest.makeSuite(VivadoExecutorTC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
