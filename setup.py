7133362869b52393d8187d3d37d0d969cb07fdbc
Nic30 <nic30@seznam.cz>
2021-06-30 23:00:03 +0200

    setup.py long_description

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+from os import path
+
 from setuptools import setup, find_packages
 
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='hwtBuildsystem',
       version='0.1',
       description='buildsystem for for HWToolkit (hwt, the fpga devel. library)',
+      long_description=long_description,
       url='https://github.com/Nic30/hwtBuildsystem',
       author='Michal Orsak',
       author_email='Nic30original@gmail.com',
