f41242894efbc0f8ff7c0f57c7eff7a53e68d0d9
Nic30 <nic30@seznam.cz>
2021-09-27 23:15:03 +0200

    unit_from_cli_args: fix param iteration

--- hwtBuildsystem/cli_utils/unit_from_cli_args.py
+++ hwtBuildsystem/cli_utils/unit_from_cli_args.py
@@ -1,7 +1,9 @@
 
 import argparse
 import inspect
+from io import StringIO
 import os
+import sys
 from typing import Type, Optional, List
 
 from hwt.serializer.store_manager import SaveToSingleFiles
@@ -11,12 +13,18 @@ from hwt.synthesizer.utils import to_rtl
 from hwtLib.examples.hierarchy.multiConfigUnit import MultiConfigUnitWrapper
 
 
-def unit_from_cli_args(unitCls: Type, args:Optional[List[str]]=None, out_folder:Optional[str] = None, unit_name:Optional[str] = None):
+def unit_from_cli_args(unitCls: Type,
+                       args:Optional[List[str]]=None,
+                       out_folder:Optional[str]=None,
+                       unit_name:Optional[str]=None,
+                       stdout:StringIO=None):
     """
     :param unitCls: unit class or anything callable which produces instance of Unit
     :param args: list of CLI arguments, if None the CLI args of this python execution are used
     """
     defInstance = unitCls()
+    if stdout is None:
+        stdout = sys.stdout
 
     parser = argparse.ArgumentParser('Generate hwt component files from specification of possible parameter/generic values')
     parser.add_argument('-f', '--files', action='store_true', help='Print all source absolute file paths')
@@ -29,30 +37,40 @@ def unit_from_cli_args(unitCls: Type, args:Optional[List[str]]=None, out_folder:
     args = parser.parse_args(args=args)
 
     if(args.component == True):
-        print(defInstance._getDefaultName())
+        stdout.write(str(defInstance._getDefaultName()))
+        return
+
+    if(args.generics == True):
+        stdout.write(str(' '.join([p._name for p in defInstance._params])))
         return
 
     unitConfigs = []
-    has_next = True
-    while has_next:
-        has_next = False
-        curIndex = len(unitConfigs)
+    configs_cnt = None
+    for p in defInstance._params:
+        v = getattr(args, p._name)
+        if configs_cnt is None:
+            configs_cnt = len(v)
+        else:
+            assert configs_cnt == len(v), ("All paramenters must have same number of values,"
+                                           " because each item represents a value for specific combination")
+    assert configs_cnt is not None, "At least some parameters needs to be specified"
+    for i in range(configs_cnt):
         curInst = unitCls()
+
         for p in defInstance._params:
             v = getattr(args, p._name)
-            if(len(v) > curIndex):
-                setattr(curInst, p._name, v[curIndex])
-            if(len(v) > curIndex + 1):
-                has_next = True
+            v = p.get_value().__class__(v[i])
+            setattr(curInst, p._name, v)
+
         unitConfigs.append(curInst)
 
     if(unit_name is None):
         unitFile = inspect.getfile(defInstance.__class__)
         unit_name = os.path.splitext(os.path.basename(unitFile))[0]
-        
+
     if(out_folder is None):
         out_folder = os.getcwd()
-        
+
     rtl_dir_path = os.path.join(out_folder,
                                 unit_name)
     serializers = {
@@ -63,10 +81,6 @@ def unit_from_cli_args(unitCls: Type, args:Optional[List[str]]=None, out_folder:
     multiConfUnit = MultiConfigUnitWrapper(unitConfigs)
     to_rtl(multiConfUnit, store_manager=store_man)
 
-    if(args.generics == True):
-        print(' '.join([p._name for p in defInstance._params]))
-        return
-
     if(args.files == True):
-        print(' '.join(list(filter(None, store_man.files))))
+        stdout.write(str(' '.join(list(filter(None, store_man.files)))))
         return
