65738e61c6211cc4dcf2ae74c06be94b0c5aba2b
Nic30 <nic30@seznam.cz>
2021-09-27 23:14:51 +0200

    paramEmptyUnit.vhd: fix reference file

--- tests/paramEmptyUnit.vhd
+++ tests/paramEmptyUnit.vhd
@@ -22,18 +22,18 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 ENTITY ParamEmptyUnit_1 IS
     GENERIC(
-        ADDR_WIDTH : INTEGER := 8;
+        ADDR_WIDTH : INTEGER := 16;
         DATA_WIDTH : INTEGER := 32
     );
     PORT(
-        addr : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        addr : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
         data : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE rtl OF ParamEmptyUnit_1 IS
 BEGIN
-    ASSERT ADDR_WIDTH = 8 REPORT "Generated only for this value" SEVERITY failure;
+    ASSERT ADDR_WIDTH = 16 REPORT "Generated only for this value" SEVERITY failure;
     ASSERT DATA_WIDTH = 32 REPORT "Generated only for this value" SEVERITY failure;
 END ARCHITECTURE;
 LIBRARY IEEE;
@@ -41,18 +41,18 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 ENTITY ParamEmptyUnit_2 IS
     GENERIC(
-        ADDR_WIDTH : INTEGER := 8;
+        ADDR_WIDTH : INTEGER := 32;
         DATA_WIDTH : INTEGER := 32
     );
     PORT(
-        addr : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        addr : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         data : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE rtl OF ParamEmptyUnit_2 IS
 BEGIN
-    ASSERT ADDR_WIDTH = 8 REPORT "Generated only for this value" SEVERITY failure;
+    ASSERT ADDR_WIDTH = 32 REPORT "Generated only for this value" SEVERITY failure;
     ASSERT DATA_WIDTH = 32 REPORT "Generated only for this value" SEVERITY failure;
 END ARCHITECTURE;
 LIBRARY IEEE;
@@ -64,7 +64,7 @@ ENTITY ParamEmptyUnit IS
         DATA_WIDTH : INTEGER := 32
     );
     PORT(
-        addr : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        addr : IN STD_LOGIC_VECTOR(ADDR_WIDTH - 1 DOWNTO 0);
         data : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END ENTITY;
@@ -82,21 +82,21 @@ ARCHITECTURE rtl OF ParamEmptyUnit IS
     END COMPONENT;
     COMPONENT ParamEmptyUnit_1 IS
         GENERIC(
-            ADDR_WIDTH : INTEGER := 8;
+            ADDR_WIDTH : INTEGER := 16;
             DATA_WIDTH : INTEGER := 32
         );
         PORT(
-            addr : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            addr : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
             data : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
         );
     END COMPONENT;
     COMPONENT ParamEmptyUnit_2 IS
         GENERIC(
-            ADDR_WIDTH : INTEGER := 8;
+            ADDR_WIDTH : INTEGER := 32;
             DATA_WIDTH : INTEGER := 32
         );
         PORT(
-            addr : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            addr : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
             data : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
         );
     END COMPONENT;
@@ -109,17 +109,17 @@ BEGIN
             addr => addr,
             data => data
         );
-    ELSIF ADDR_WIDTH = 8 AND DATA_WIDTH = 32 GENERATE
+    ELSIF ADDR_WIDTH = 16 AND DATA_WIDTH = 32 GENERATE
         possible_variants_1_inst: ParamEmptyUnit_1 GENERIC MAP(
-            ADDR_WIDTH => 8,
+            ADDR_WIDTH => 16,
             DATA_WIDTH => 32
         ) PORT MAP(
             addr => addr,
             data => data
         );
-    ELSIF ADDR_WIDTH = 8 AND DATA_WIDTH = 32 GENERATE
+    ELSIF ADDR_WIDTH = 32 AND DATA_WIDTH = 32 GENERATE
         possible_variants_2_inst: ParamEmptyUnit_2 GENERIC MAP(
-            ADDR_WIDTH => 8,
+            ADDR_WIDTH => 32,
             DATA_WIDTH => 32
         ) PORT MAP(
             addr => addr,
