e6623474ce5c0da1c2e721f1e793cca9c3d181fd
Nic30 <nic30@seznam.cz>
2021-10-04 22:41:03 +0200

    unit_from_cli_args: support for default configurations

--- hwtBuildsystem/cli_utils/unit_from_cli_args.py
+++ hwtBuildsystem/cli_utils/unit_from_cli_args.py
@@ -4,22 +4,63 @@ import inspect
 from io import StringIO
 import os
 import sys
-from typing import Type, Optional, List
+from typing import Type, Optional, List, Dict
 
 from hwt.serializer.store_manager import SaveToSingleFiles
 from hwt.serializer.verilog import VerilogSerializer
 from hwt.serializer.vhdl import Vhdl2008Serializer
+from hwt.synthesizer.unit import Unit
 from hwt.synthesizer.utils import to_rtl
 from hwtBuildsystem.hwt.multiConfigUnit import MultiConfigUnitWrapper
 
 
+def _parse_configs(defInstance: Unit, args: List[str], default_params: List[Dict[str, object]]):
+    configs_cnt = None
+    for p in defInstance._params:
+        v = getattr(args, p._name)
+        if configs_cnt is None:
+            configs_cnt = len(v)
+        else:
+            assert configs_cnt == len(v), ("All paramenters must have same number of values,"
+                                           " because each item represents a value for specific combination")
+    assert configs_cnt is not None, ("At least some parameters needs to be specified,"
+        " otherwise you can instanciate component directly and you do not need this function")
+
+    unitConfigs = []
+    for i in range(configs_cnt):
+        conf = {}
+        for p in defInstance._params:
+            v = getattr(args, p._name)
+            v = p.get_value().__class__(v[i])
+            conf[p._name] = v
+        unitConfigs.append(conf)
+
+    for conf in default_params:
+        assert len(conf) == len(defInstance._params), (conf, defInstance._params)
+        for p in defInstance._params:
+            v = conf[p._name]
+            v = p.get_value().__class__(v)
+            conf[p._name] = v
+
+        unitConfigs.append(conf)
+
+    unitConfigs = [tuple(sorted(c.items(), key=lambda x: x[0])) for c in unitConfigs]
+    seenConfigs = set()
+    for c in unitConfigs:
+        if c not in seenConfigs:
+            yield c
+            seenConfigs.add(c)
+
+
 def unit_from_cli_args(unitCls: Type,
+                       default_params: List[Dict[str, object]],
                        args:Optional[List[str]]=None,
                        out_folder:Optional[str]=None,
                        unit_name:Optional[str]=None,
                        stdout:StringIO=None):
     """
     :param unitCls: unit class or anything callable which produces instance of Unit
+    :param default_params: list of default parameters for a component to resolve which parameters/generics affect which port width
     :param args: list of CLI arguments, if None the CLI args of this python execution are used
     """
     defInstance = unitCls()
@@ -36,39 +77,27 @@ def unit_from_cli_args(unitCls: Type,
 
     args = parser.parse_args(args=args)
 
-    if(args.component == True):
+    if args.component:
         stdout.write(str(defInstance._getDefaultName()))
         return
 
-    if(args.generics == True):
+    if args.generics:
         stdout.write(str(' '.join([p._name for p in defInstance._params])))
         return
 
-    unitConfigs = []
-    configs_cnt = None
-    for p in defInstance._params:
-        v = getattr(args, p._name)
-        if configs_cnt is None:
-            configs_cnt = len(v)
-        else:
-            assert configs_cnt == len(v), ("All paramenters must have same number of values,"
-                                           " because each item represents a value for specific combination")
-    assert configs_cnt is not None, "At least some parameters needs to be specified"
-    for i in range(configs_cnt):
+    unitVariants = []
+    for conf in _parse_configs(defInstance, args, default_params):
         curInst = unitCls()
+        for param_name, param_value in conf:
+            setattr(curInst, param_name, param_value)
 
-        for p in defInstance._params:
-            v = getattr(args, p._name)
-            v = p.get_value().__class__(v[i])
-            setattr(curInst, p._name, v)
+        unitVariants.append(curInst)
 
-        unitConfigs.append(curInst)
-
-    if(unit_name is None):
+    if unit_name is None:
         unitFile = inspect.getfile(defInstance.__class__)
         unit_name = os.path.splitext(os.path.basename(unitFile))[0]
 
-    if(out_folder is None):
+    if out_folder is None:
         out_folder = os.getcwd()
 
     rtl_dir_path = os.path.join(out_folder,
@@ -78,9 +107,9 @@ def unit_from_cli_args(unitCls: Type,
         "sv2012": VerilogSerializer,
     }
     store_man = SaveToSingleFiles(serializers[args.language], rtl_dir_path, name=unit_name)
-    multiConfUnit = MultiConfigUnitWrapper(unitConfigs)
+    multiConfUnit = MultiConfigUnitWrapper(unitVariants)
     to_rtl(multiConfUnit, store_manager=store_man)
 
-    if(args.files == True):
+    if args.files:
         stdout.write(str(' '.join(list(filter(None, store_man.files)))))
         return
--- tests/cli_utils_test.py
+++ tests/cli_utils_test.py
@@ -13,7 +13,10 @@ from hwt.synthesizer.param import Param
 from hwtBuildsystem.cli_utils.unit_from_cli_args import unit_from_cli_args
 
 
+
 class ParamEmptyUnit(EmptyUnit):
+    DEFAULT_ARGS = [{"ADDR_WIDTH": 8, "DATA_WIDTH":32},
+                    {"ADDR_WIDTH": 16, "DATA_WIDTH":32}]
 
     def _config(self):
         self.ADDR_WIDTH = Param(8)
@@ -28,13 +31,13 @@ class CLIUtilsTC(TestCase):
 
     def test_unit_from_cli_args_component_list(self):
         f = io.StringIO()
-        unit_from_cli_args(ParamEmptyUnit, ["-c"], stdout=f)
+        unit_from_cli_args(ParamEmptyUnit, ParamEmptyUnit.DEFAULT_ARGS, ["-c"], stdout=f)
         output = f.getvalue()
         self.assertEqual(output.strip(), "ParamEmptyUnit")
 
     def test_unit_from_cli_args_generic_list(self):
         f = io.StringIO()
-        unit_from_cli_args(ParamEmptyUnit, ["-g"], stdout=f)
+        unit_from_cli_args(ParamEmptyUnit, ParamEmptyUnit.DEFAULT_ARGS, ["-g"], stdout=f)
         output = f.getvalue()
         self.assertEqual(output.strip(), "ADDR_WIDTH DATA_WIDTH")
 
@@ -43,6 +46,7 @@ class CLIUtilsTC(TestCase):
         with tempfile.TemporaryDirectory() as dirpath:
 
             unit_from_cli_args(ParamEmptyUnit,
+                               ParamEmptyUnit.DEFAULT_ARGS,
                                ["-f", "-l", "vhdl2008"],
                                out_folder=dirpath,
                                stdout=f)
@@ -55,6 +59,7 @@ class CLIUtilsTC(TestCase):
         with tempfile.TemporaryDirectory() as dirpath:
 
             unit_from_cli_args(ParamEmptyUnit,
+                               ParamEmptyUnit.DEFAULT_ARGS,
                                ["-f", "-l", "vhdl2008"],
                                out_folder=dirpath, unit_name="paramEmptyUnit",
                                                stdout=f)
@@ -67,6 +72,7 @@ class CLIUtilsTC(TestCase):
         with tempfile.TemporaryDirectory() as dirpath:
 
             unit_from_cli_args(ParamEmptyUnit,
+                               ParamEmptyUnit.DEFAULT_ARGS,
                                ["-f", "-l", "sv2012"],
                                out_folder=dirpath, unit_name="paramEmptyUnit",
                                stdout=f)
@@ -77,17 +83,19 @@ class CLIUtilsTC(TestCase):
     def test_unit_from_cli_args_basic_generation(self):
         f = io.StringIO()
         with tempfile.TemporaryDirectory() as dirpath:
-            unit_from_cli_args(ParamEmptyUnit, ["-l", "vhdl2008",
-                                               "--ADDR_WIDTH", "8", "16", '32',
-                                               "--DATA_WIDTH", "32", "32", "32"],
-                                               out_folder=dirpath, unit_name="paramEmptyUnit",
-                                               stdout=f)
+            unit_from_cli_args(ParamEmptyUnit,
+                               ParamEmptyUnit.DEFAULT_ARGS,
+                               ["-l", "vhdl2008",
+                                "--ADDR_WIDTH", "8", "16", '32',
+                                "--DATA_WIDTH", "32", "32", "32"],
+                               out_folder=dirpath, unit_name="paramEmptyUnit",
+                               stdout=f)
             output = f.getvalue()
             self.assertEqual(output.strip(), "")
             outFile = os.path.join(dirpath, "paramEmptyUnit/paramEmptyUnit.vhd")
             refFileName = os.path.join(os.path.dirname(__file__), "paramEmptyUnit.vhd")
 
-            #with open(refFileName, 'w') as f, open(outFile) as of:
+            # with open(refFileName, 'w') as f, open(outFile) as of:
             #    f.write(of.read())
 
             with open(outFile) as of, open(refFileName) as refFile:
